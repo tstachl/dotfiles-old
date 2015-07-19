6a6df3807b887130d28f3db498c1f813805b1eb1
Thomas Stachl <thomas@stachl.me>
2015-07-19 15:59:31 -0700

    forgot to save dotvim.helper.fish

---
+++ fish/plugins/dotvim/dotvim.helper.fish
@@ -0,0 +1,13 @@
+# NAME
+#   dotvim.helper - Prints dotvim help
+#
+# DESCRIPTION
+#   Prints all functions supported by dotvim helper
+#
+function dotvim.helper -d 'Prints all functions supported by dotvim helper'
+  omf.log normal 'Dotvim is a framework for setting up vim and plugins'
+  omf.log normal 'using vundle.'
+  omf.log normal ''
+  omf.log normal '  Examples:'
+  omf.log normal '    dotvim self-update'
+end
