634749e6d4c73abd6862e95fe83241b8f9165253
Thomas Stachl <thomas@stachl.me>
2015-07-19 16:08:49 -0700

    include dotvim in config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,12 @@
 # Path to your dotfiles.
-set dot_path          (dirname (dirname (readlink (status --current-filename))))
+if test -L (status --current-filename)
+  set -x dot_path    (dirname (dirname (readlink (status --current-filename))))
+else
+  set -x dot_path    (dirname (dirname (status --current-filename)))
+end
+
+# Path to dotvim
+set dotvim_path       $dot_path/dotvim
 
 # Path to your oh-my-fish.
 set fish_path         $dot_path/oh-my-fish
@@ -17,10 +24,12 @@ set default_user      'tstachl'
 # Plugins and themes can be found at https://github.com/oh-my-fish/
 Theme 'thomasstachl'
 Plugin 'dotfiles'
+Plugin 'dotvim'
 
 # Make sure we're up to date
 omf self-update
 dotfiles self-update
+dotvim self-update
 
 # Add all the dotfiles
 dotfiles link git/gitattributes
@@ -28,3 +37,5 @@ dotfiles link git/gitconfig
 dotfiles link git/gitignore
 dotfiles link git/gitk
 dotfiles link git/tigrc
+
+dotfiles link vim/vimrc
